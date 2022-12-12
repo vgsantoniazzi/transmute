14adabe076156c6870894a97880d69575ff17268
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2022-12-11 23:33:47 -0300

    Remove class definition from operations

--- engine/src/file/ruby.rs
+++ engine/src/file/ruby.rs
@@ -8,7 +8,7 @@ use crate::file::MutableItem;
 static STRING_CHARSET: &str = "abcdefghijklmnopqrstuvwxyz_";
 static NUMBER_CHARSET: &str = "1234567890";
 static OPERATOR_CHARSET: [&str; 6] = [">", "<", "<=", ">=", "==", "!="];
-static EQ_OPERATOR_CHARSET: [&str; 2] = ["==", "!="];
+static EQUALITY_OPERATOR_CHARSET: [&str; 2] = ["==", "!="];
 
 pub fn find_all(file_path: &str) -> Vec<MutableItem> {
     trace!("searching all mutable items for file {}", file_path);
@@ -56,12 +56,23 @@ fn find_mutable_items(string: &str) -> Vec<(String, String)> {
         ));
     }
 
+    for result in find_less_than_operators(string) {
+        items.push((
+            result.to_string(),
+            format!(
+                r#"{}"#,
+                OPERATOR_CHARSET.iter().find(|&&x| x != result).unwrap()
+            ),
+        ));
+    }
+
+
     for result in find_eq_operators(string) {
         items.push((
             result.to_string(),
             format!(
                 r#"{}"#,
-                EQ_OPERATOR_CHARSET.iter().find(|&&x| x != result).unwrap()
+                EQUALITY_OPERATOR_CHARSET.iter().find(|&&x| x != result).unwrap()
             ),
         ));
     }
@@ -81,10 +92,10 @@ fn find_strings(string: &str) -> Vec<String> {
 
 fn find_symbols(string: &str) -> Vec<String> {
     let mut result: Vec<String> = Vec::new();
-    let regex = Regex::new(r#":(\w+)"#).unwrap();
+    let regex = Regex::new(r#"([( ])(?P<symbol>:\w+)"#).unwrap();
     for cap in regex.captures_iter(string) {
-        trace!("Symbol {} found", &cap[0]);
-        result.push(cap[0].to_string())
+        trace!("Symbol {} found", &cap["symbol"]);
+        result.push(cap["symbol"].to_string())
     }
     return result;
 }
@@ -101,10 +112,24 @@ fn find_numbers(string: &str) -> Vec<String> {
 
 fn find_operators(string: &str) -> Vec<String> {
     let mut result: Vec<String> = Vec::new();
-    let regex = Regex::new(r#"(>|<|>=|<=)"#).unwrap();
+    let regex = Regex::new(r#"(>|>=|<=)"#).unwrap();
     for cap in regex.captures_iter(string) {
-        trace!("Operator {} found", &cap[0]);
-        result.push(cap[0].to_string())
+        if !string.contains("class") {
+            trace!("Operator {} found", &cap[0]);
+            result.push(cap[0].to_string())
+        }
+    }
+    return result;
+}
+
+fn find_less_than_operators(string: &str) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    let regex = Regex::new(r#"(<)"#).unwrap();
+    for cap in regex.captures_iter(string) {
+        if !string.contains("class") {
+            trace!("Operator {} found", &cap[0]);
+            result.push(cap[0].to_string())
+        }
     }
     return result;
 }
